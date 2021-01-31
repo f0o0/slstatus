42da6a71736122591b18d8af5d81f6f977ea8141
f0o0 <stephan.bliedung@gmail.com>
2021-01-31 08:49:49 +0100

    config.h battery icon

--- config.h
+++ config.h
@@ -68,4 +68,5 @@ static const struct arg args[] = {
 	{ datetime, "%s",           "%F %T" },
  	{ cpu_perc, "[CPU %s%%]", NULL },
  	{ ram_perc, "[RAM %s%%]", NULL },
+	{ battery_perc, "[ %s%%]", NULL },
 };
