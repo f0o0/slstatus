af232be7c6a1a3c86fd8e0d31935b7512a63a23a
f0o0 <stephan.bliedung@gmail.com>
2021-01-31 07:43:30 +0100

    statusbar CPU, RAM, battery

--- config.def.h
+++ config.def.h
@@ -66,4 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ datetime, "%s",           "%F %T" },
+	{ battery_perc, "[ %s%%]" , NULL ),
+ 	{ cpu_perc, "[CPU %s%%]", NULL },
+ 	{ ram_perc, "[RAM %s%%]", NULL },
 };
